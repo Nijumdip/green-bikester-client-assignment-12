051b68c4c6470d37cc24fae1b7d59ab5e368073c
Nijumdip <nijumdip019911@gmail.com>
2022-05-22 11:43:12 +0600

    banner added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            Home Pages
+            <Banner></Banner>
             {/* <button className="btn w-64 rounded-full bg-emerald-900">Button</button> */}
         </div>
     );
