4797bcbb686ecb2a51e3a4f21db213140ebef7f7
Nijumdip <nijumdip019911@gmail.com>
2022-05-22 11:27:11 +0600

    navbar added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Home Pages
+            {/* <button className="btn w-64 rounded-full bg-emerald-900">Button</button> */}
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Page404.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Page404 = () => {
+    return (
+        <div className='flex flex-col text-4xl container my-10 justify-center items-center'>
+            <h1> SORRY ! </h1>
+            <h1>THIS PAGE IS NOT FOUND ....</h1>
+            
+            <img
+                className=''
+                width='350px' 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUIHsc7ygCKNEyFFCU8x-AIo-SMK43gUjAuQ&usqp=CAU" alt="" />
+        </div>
+    );
+};
+
+export default Page404;
