3b288533f3de39cb7aa846433ac2e78a4e4f0eb4
Nijumdip <nijumdip019911@gmail.com>
2022-05-22 21:08:47 +0600

    added hot product page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "./Banner";
 import BusinessSummary from "./BusinessSummary";
+import HotProducts from "./HotProducts";
 import Review from "./Review";
 import WhyChoose from "./WhyChoose";
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
           <Banner></Banner>
           <BusinessSummary></BusinessSummary>
+          <HotProducts></HotProducts>
           <Review></Review>
           <WhyChoose></WhyChoose>
     </div>
