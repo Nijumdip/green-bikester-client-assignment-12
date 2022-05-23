5dbcf5875b6cabf9ea3a047fcbafcb99292a95d2
Nijumdip <nijumdip019911@gmail.com>
2022-05-23 23:34:09 +0600

    loading login form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,146 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import auth from "../../firebase.int";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+// import Loading from "../Shared/Loading";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Loading from "../Shared/Loading";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>Login</h2>
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const {register,formState: { errors },handleSubmit} = useForm();
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  //   const [token]=useToken(user || gUser)
+
+  let signInError;
+  let navigate = useNavigate();
+  let location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (gUser || user) {
+      navigate(from, { replace: true });
+    }
+  }, [gUser, user, from, navigate]);
+
+    if (loading || gLoading) {
+      return <Loading></Loading>;
+    }
+
+    if (error || gError) {
+      signInError = (
+        <p className="text-red-500">{error?.message || gError?.message}</p>
+      );
+    }
+
+  const onSubmit = (data) => {
+    signInWithEmailAndPassword(data.email, data.password);
+  };
+  return (
+    <div>
+      <div className="flex h-screen justify-center items-center">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h1 className="text-2xl font-bold text-emerald-900">Login</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Your Email"
+                  className="input input-bordered w-full max-w-xs"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Email is Required",
+                    },
+                    pattern: {
+                      value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                      message: "Provide a valid Email",
+                    },
+                  })}
+                />
+                <label className="label">
+                  {errors.email?.type === "required" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.email.message}
+                    </span>
+                  )}
+
+                  {errors.email?.type === "pattern" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.email.message}
+                    </span>
+                  )}
+                </label>
+              </div>
+
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Your password"
+                  className="input input-bordered w-full max-w-xs"
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Password is Required",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Must be 6 characters or longer",
+                    },
+                  })}
+                />
+                <label className="label">
+                  {errors.password?.type === "required" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.password.message}
+                    </span>
+                  )}
+
+                  {errors.password?.type === "minLength" && (
+                    <span className="label-text-alt text-red-500">
+                      {errors.password.message}
+                    </span>
+                  )}
+                </label>
+              </div>
+              {signInError}
+              <input
+                className="btn w-full max-w-xs bg-emerald-900"
+                value="login"
+                type="submit"
+              />
+            </form>
+            <p>
+              <small>
+                New to Green Bikester ?{" "}
+                <Link className="text-primary" to="/signup">
+                  Create New Account
+                </Link>
+              </small>
+            </p>
+            <div className="divider">OR</div>
+            <button
+              onClick={() => signInWithGoogle()}
+              className="btn btn-outline hover:bg-emerald-900"
+            >
+              Continue with Google
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/firebase.int.js
+++ src/firebase.int.js
@@ -4,20 +4,20 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     
-    apiKey: process.env.REACT_APP_API_KEY,
+    /* apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    appId: process.env.REACT_APP_APP_ID, */
   
- /*  apiKey: "AIzaSyCYUdiiY98CbegbrajtbNvbI-7bvaA38ow",
+  apiKey: "AIzaSyCYUdiiY98CbegbrajtbNvbI-7bvaA38ow",
   authDomain: "green-bikester-b23b0.firebaseapp.com",
   projectId: "green-bikester-b23b0",
   storageBucket: "green-bikester-b23b0.appspot.com",
   messagingSenderId: "769869616091",
   appId: "1:769869616091:web:755ad5183ac65ab9874dfe", 
-  */
+  
 };
 
 
